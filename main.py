073dbea1b4e6572909171969e70810338fdac301
samed <cabbarlisemed@gmail.com>
2021-12-07 10:12:46 +0400

    last

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from keras.optimizers import rmsprop_v2
 import matplotlib.pyplot as plt
 
 (x_tr, y_tr), (x_test, y_test) = mnist.load_data()
+a = x_test[1]
 num_classes = 10
 batch_size = 128
 epochs = 150
+
 x_tr = x_tr.reshape(60000, 784)
 x_test = x_test.reshape(10000,784)
 x_tr = x_tr.astype('float32')
@@ -36,8 +38,11 @@ model.compile(loss='categorical_crossentropy', optimizer=rmsprop_v2.RMSProp(), m
 
 history = model.fit(x_tr, y_tr, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test))
 score = model.evaluate(x_test, y_test)
+
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
-prediction = model.predict(x_test[0])
+prediction = model.predict(x_test)
+print(y_test[1])
+plt.imshow(a)
 plt.plot(prediction, y_test, 'o')
 plt.show()
